8b511284e46f912cf67914989a12dde36c179d6b
Dmitrii Nikolaev <dmnikola@amd.com>
2026-01-14 13:34:13 +0200

    refactor

--- run_nemo_all.sh
+++ run_nemo_all.sh
@@ -1,1 +1,2 @@
-    python3 benchmark.py --model llama --parallel "memory_optimized" --output-dir output_04
+    python3 benchmark.py --parallel "memory_optimized" --output-dir output-04
+    python3 benchmark.py --parallel "balanced" --output-dir output-05
