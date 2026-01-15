f0318635e123afa03df8c7bc4a25285d9ae1b81f
Dmitrii Nikolaev <dmnikola@amd.com>
2026-01-15 11:13:34 +0200

    refactored

--- run_nemo_all.sh
+++ run_nemo_all.sh
@@ -1,2 +1,10 @@
-    python3 benchmark.py --parallel "memory_optimized" --output-dir output-04
-    python3 benchmark.py --parallel "balanced" --output-dir output-05
+#!/bin/bash
+python3 benchmark.py --parallel "maximum_performance" --output-dir output-00
+sleep 10
+python3 benchmark.py --parallel "identical_config" --output-dir output-01
+sleep 10
+python3 benchmark.py --parallel "memory_optimized" --output-dir output-02
+sleep 10
+python3 benchmark.py --parallel "minimal_communication" --output-dir output-03
+sleep 10
+python3 benchmark.py --parallel "balanced" --output-dir output-04
--- run_primus_all.sh
+++ run_primus_all.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 OUTPUT_DIR=output-00 TPRIMAT_PARALLEL=maximum_performance ./run_primus_model.sh
+sleep 10
 OUTPUT_DIR=output-01 TPRIMAT_PARALLEL=identical_config ./run_primus_model.sh
+sleep 10
 OUTPUT_DIR=output-02 TPRIMAT_PARALLEL=memory_optimized ./run_primus_model.sh
-OUTPUT_DIR=output-04 TPRIMAT_PARALLEL=minimal_communication ./run_primus_model.sh
-OUTPUT_DIR=output-05 TPRIMAT_PARALLEL=balanced ./run_primus_model.sh
+sleep 10
+OUTPUT_DIR=output-03 TPRIMAT_PARALLEL=minimal_communication ./run_primus_model.sh
+sleep 10
+OUTPUT_DIR=output-04 TPRIMAT_PARALLEL=balanced ./run_primus_model.sh
