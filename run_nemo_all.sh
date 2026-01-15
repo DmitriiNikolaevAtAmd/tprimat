a17883a43639c2276d0ec35749eeccf450d5b798
Dmitrii Nikolaev <dmnikola@amd.com>
2026-01-16 01:14:39 +0200

    refactored

--- run_nemo_all.sh
+++ run_nemo_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-python3 benchmark.py --parallel "maximum_performance" --output-dir output-00
+python3 benchmark.py --parallel "maximum_performance" --output-dir output/output-00
 sleep 10
-python3 benchmark.py --parallel "identical_config" --output-dir output-01
+python3 benchmark.py --parallel "identical_config" --output-dir output/output-01
 sleep 10
-python3 benchmark.py --parallel "memory_optimized" --output-dir output-02
+python3 benchmark.py --parallel "memory_optimized" --output-dir output/output-02
 sleep 10
-python3 benchmark.py --parallel "minimal_communication" --output-dir output-03
+python3 benchmark.py --parallel "minimal_communication" --output-dir output/output-03
 sleep 10
-python3 benchmark.py --parallel "balanced" --output-dir output-04
+python3 benchmark.py --parallel "balanced" --output-dir output/output-04
