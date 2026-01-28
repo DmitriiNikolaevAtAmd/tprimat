c8b5dd1eb681e81cc2ee8a004917b79b03062082
Dmitrii Nikolaev <dmnikola@amd.com>
2026-01-28 22:07:23 +0200

    refactored

--- train/amd_mega_qwen.sh
+++ train/amd_mega_qwen.sh
@@ -6,7 +6,6 @@ OUTPUT_DIR="./output"
 mkdir -p "$OUTPUT_DIR"
 export HF_HOME="./cache"
 mkdir -p "$HF_HOME"
-export PYTORCH_ALLOC_CONF=expandable_segments:True
 export HSA_NO_SCRATCH_RECLAIM=1
 export HSA_ENABLE_SDMA=1
 export NCCL_DEBUG=INFO
