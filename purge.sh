d0d1310f919a69dd54fc7a1323026bddb90c31cd
Dmitrii Nikolaev <dmnikola@amd.com>
2026-01-29 09:09:36 +0200

    refactored

--- purge.sh
+++ purge.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# WARNING: This will delete local output and cache directories
-# It will NOT delete /data/tprimat (external mounted volume)
-rm -rf cache output __pycache__ torchelastic_* *.out *.err *.zip
-echo "Cleaned up local cache and output directories"
-echo "Note: /data/tprimat is preserved (external volume)"
+rm -rf cache output __pycache__ torchelastic_* *.out *.err *.zip /data/tprimat
